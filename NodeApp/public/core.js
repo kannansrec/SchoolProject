4abbcaefbbe57d3f8fb032f28383cc35ab5dc79c
bharath063 <bharath@macbookair.local>
2015-06-14 12:09:43 +0530

    created client
    The client.html file currently displays the pending trip details on
    page load. Still need to do server.js to set status to pending on
    addRecord action and set it to ‘complete’ on updateRecord action on the
    table ‘trip_details’. also need to add pop up windows for adding new
    record and editing pending records.

--- NodeApp/public/core.js
+++ NodeApp/public/core.js
@@ -1,9 +1,16 @@
+/*
+contains the angular.js core javascript file for running on client side
+*/
+
 var app = angular.module('myapp', []);
 app.controller('myctrl',function($scope, $http) {
-        $scope.formData = { id:'DRV1234' + new Date().getSeconds(), driver_name: 'testData', driver_licence_no : '21312' , driver_mobile_no : '123', driver_address:'asdas'};
-        // when landing on the page, get all todos and show them
-    
-    $http.get('/api/driver_info')
+        $scope.formData = { id:'DRV1234' + new Date().getSeconds(), driver_name: 'testData', driver_licence_no : '21312' , driver_mobile_no : '123', driver_address:'asdas',tablename: document.getElementById('tablename')};
+        
+        tablename=tablename.value;
+      
+        // when the page is loaded, invoke this to display data
+   alert("tablename: "+tablename);
+    $http.get('/api/'+tablename)
             .success(function(data) {
                 $scope.records = data;
                 console.log(data);
@@ -11,9 +18,13 @@ app.controller('myctrl',function($scope, $http) {
             .error(function(data) {
                 console.log('Error: ' + data);
             });
+  
     
-        $scope.getRecords = function(tablename) {
-         tablename ='driver_info';
+
+    
+    
+        $scope.getRecords = function(tablname) {
+//         tablename ='driver_info';
         $http.get('/api/'+tablename)
             .success(function(data) {
                 $scope.records = data;
@@ -23,7 +34,7 @@ app.controller('myctrl',function($scope, $http) {
                 console.log('Error: ' + data);
             });
      };
-
+    	
         // when submitting the add form, send the text to the node API
         $scope.createRecord = function(tablename) {
             $http.post('/api/'+tablename, $scope.formData)
@@ -36,7 +47,9 @@ app.controller('myctrl',function($scope, $http) {
                 });
         };
 
-        // delete a todo after checking it
+//         delete a record on clicking trash icon
+//         @params tablename: name of the table
+//         @params id: id key value to be deleted
         $scope.deleteRecord = function(tablename, id) {
             $http.delete('/api/'+tablename+'/'+ id)
                 .success(function(data) {
@@ -47,6 +60,8 @@ app.controller('myctrl',function($scope, $http) {
                     console.log('Error: ' + data);
                 });
         };
+        
+        
         // Update a todo after checking it
         $scope.updateRecord = function(tablename,id,id_data) {
             $scope.formData = id_data;
@@ -60,3 +75,5 @@ app.controller('myctrl',function($scope, $http) {
         };
 
     });
+    
+    
